bf8414483d333e2c7a3ca2a0f8667cf4502f10a2
afnan waseem <afnanwaseem26@gmail.com>
2025-11-18 20:08:47 +0500

    "files updates"

--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -11,7 +11,7 @@ class CartController extends Controller
      */
     public function index()
     {
-        return view("website.shopcart");
+        return view("website.cart");
     }
 
     /**
